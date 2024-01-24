3b05f7934b98d4894a2a4e9f1104a8644e7b8822
unknown <tryphena2390@gmail.com>
2024-01-24 22:02:17 +0530

    first commit

---
+++ components/social-links.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const SocialMediaLinks = () => {
+    return (
+        <div className='flex flex-row gap-4'>
+          
+            <a href="https://www.linkedin.com/in/harshan-nayak-020754285/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+        </div>
+    );
+};
+
+export default SocialMediaLinks;
